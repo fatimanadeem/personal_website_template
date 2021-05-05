8d0034158768df7c36cf32a97b30809f3e95c5b5
Fatima Nadeem <fatima.nadeem@mail.utoronto.ca>
2021-05-05 19:29:18 -0400

    All resources

---
+++ src/resumeData.js
@@ -0,0 +1,156 @@
+// eslint-disable-next-line
+let resumeData =
+{
+  "name": "Jane Doe",
+  "description": "Here will be a description that you'll use to attract potentional employers. For example: I'm an aspiring Data Scientist that also enjoys frontend development. I enjoy creating software solutions that aim to solve realworld problems and that give back to my community. Scroll down to learn more about me!",
+  "image": "./images/profile-picture.png",
+  "bio": "Use the bio section as a way to provide more information about yourself, anything you'd like people to know about you. For example what year you're in, what your current interest are, hobbies, etc. ",
+  "email": "your.email@gmail.com",
+  "phone": "123-456-7890",
+  "address": {
+    "city": "[YOUR CITY]",
+    "province": "[YOUR PROVINCE]",
+    "Country": "[YOUR COUNTRY]",
+  },
+  "resumedownload": "https://github.com/fatimanadeem/personal_website_template", // Link to where they can access your resume, could be a google drive link or you can host pdfs on https://pdfhost.io/
+  // If there are some socials you'd don't want to include simply remove that chunk. Eg. Removing lines 18 - 22 
+  "socials": [
+    {
+      "name": "linkedin",
+      "url": "https://www.linkedin.com/",
+      "className": "fa fa-linkedin-square"
+    },
+    {
+      "name": "github",
+      "url": "http://github.com/",
+      "className": "fa fa-github"
+    },
+    {
+      "name": "facebook",
+      "url": "http://facebook.com/",
+      "className": "fa fa-facebook-square"
+    },
+    {
+      "name": "instagram",
+      "url": "http://instagram.com/",
+      "className": "fa fa-instagram"
+    },
+    {
+      "name": "skype",
+      "url": "http://skype.com/",
+      "className": "fa fa-skype"
+    },
+    {
+      "name": "twitter",
+      "url": "http://twitter.com/",
+      "className": "fa fa-twitter"
+    },
+    {
+      "name": "behance",
+      "url": "http://behance.com/",
+      "className": "fa fa-behance"
+    },
+    {
+      "name": "dribble",
+      "url": "http://dribble.com/",
+      "className": "fa fa-dribbble"
+    }
+  ],
+  "education": [
+    {
+      "school": "University Name",
+      "degree": "Undergrad in Pulling All Nighters",
+      "duration": "Sept 2019 - Present",
+      "description": "Describe your experience at school, what you learned, what useful skills you have acquired etc."
+    }
+    // If you have more education experience then just copy the section above and repeat for as many experience.
+  ],
+  "work": [
+    {
+      "company": "Awesome Company #1",
+      "title": "Master of Googling Answers",
+      "duration": "March 2010 - Present",
+      "description": "Describe work, special projects, notable achievements, what technologies you have been working with, and anything else that would be useful for an employer to know."
+    },
+    {
+      "company": "Ok Company #2",
+      "title": "Junior in Copying Stack Overflow Answers",
+      "duration": "March 20019 - February 2020",
+      "description": "Describe work, special projects, notable achievements, what technologies you have been working with, and anything else that would be useful for an employer to know."
+    }
+    // If you have more or less work experience then just copy the section above and repeat for as many experience.
+  ],
+  "extracurricular": [
+    {
+      "activity": "Cool Club #1",
+      "role": "Vice President of Being Awesome ",
+      "duration": "March 2019 - Present",
+      "responsibilities": "Describe work, special projects, notable achievements, what technologies you have been working with, and anything else that would be useful for an employer to know."
+    }
+  ],
+  "skills": [
+    // Here you can list off your top technical skill. I'd recommend at most 10, however 4-8 is a good range.
+    {
+      "name": "Git",
+      "level": "60%"
+    },
+    {
+      "name": "Illustrator",
+      "level": "20%"
+    },
+    {
+      "name": "ReactJs",
+      "level": "50%"
+    },
+    {
+      "name": "CSS",
+      "level": "90%"
+    },
+    {
+      "name": "HTML5",
+      "level": "80%"
+    },
+    {
+      "name": "VueJs",
+      "level": "50%"
+    },
+    {
+      "name": "MongoDB",
+      "level": "80%"
+    }
+  ],
+  "projects": [
+    {
+      "title": "Lorem ipsum dolor sit amet.",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      "image": "./images/portfolio/g-sanddunes.jpg",
+      "url": "https://github.com/fatimanadeem/personal_website_template"
+    },
+    {
+      "title": "Lorem ipsum dolor sit amet.",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      "image": "./images/portfolio/g-shutterbug.jpg",
+      "url": "https://github.com/fatimanadeem/personal_website_template"
+    },
+    {
+      "title": "Lorem ipsum dolor sit amet.",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      "image": "./images/portfolio/g-skaterboy.jpg",
+      "url": "https://github.com/fatimanadeem/personal_website_template"
+    },
+    {
+      "title": "Lorem ipsum dolor sit amet.",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      "image": "./images/portfolio/g-fuji.jpg",
+      "url": "https://github.com/fatimanadeem/personal_website_template"
+    },
+    {
+      "title": "Lorem ipsum dolor sit amet.",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      "image": "./images/portfolio/g-droplet.jpg",
+      "url": "https://github.com/fatimanadeem/personal_website_template"
+    },
+  ]
+}
+
+export default resumeData;
